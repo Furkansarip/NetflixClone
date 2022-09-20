a4f72b904e67158e3617dec254cfb8511790e96c
Furkan Sarı <furkansarip@gmail.com>
2022-09-20 15:35:51 +0300

    SceneDelegate settings and Creating TabBar

---
+++ NetflixClone/NetflixClone/Controllers/SearchViewController.swift
@@ -0,0 +1,21 @@
+//
+//  SearchViewController.swift
+//  NetflixClone
+//
+//  Created by Furkan Sarı on 20.09.2022.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemBackground
+    }
+    
+
+   
+
+}
---
+++ NetflixClone/NetflixClone/Controllers/UpcomingViewController.swift
@@ -0,0 +1,21 @@
+//
+//  UpcomingViewController.swift
+//  NetflixClone
+//
+//  Created by Furkan Sarı on 20.09.2022.
+//
+
+import UIKit
+
+class UpcomingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemBackground
+    }
+    
+
+   
+
+}
